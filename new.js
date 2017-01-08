9d9322e63a8f91a2fb13f4cb5038b0c06cfd361f
Ashish <ashish240284@gmail.com>
2017-01-08 23:11:46 +0530

    Removed commented code

--- new.js
+++ new.js
@@ -10,19 +10,6 @@ newApp.set('view engine', 'ejs');
 
 newRouter = express.Router();
 
-newRouter.param('name', function (req, res, next, name) {
-    console.log('doing name validation on '+ name);
-   req.name = name;
-    next();
-});
-newRouter.get('/handler/:name', function (req, res) {
-    res.send("Hello "+ req.name + '!');
-});
-
-newApp.get('/about', function (req, res) {
-    res.render("pages/about");
-});
-
 newApp.get('/', function (req, res) {
     var drinks = [
         {name: 'Coca Cola', drunkness: 1},
@@ -43,17 +30,3 @@ newApp.use('/', newRouter);
 
 
 
-/*newApp.use(express.static('public'));
-
-newRouter.get('/about', function (req, res) {
-  //  res.send("This is about page")
-//});
-newRouter.get('/contact', function (req, res) {
-    res.send("This is contact page")
-});
-
-newApp.use(newRouter);
-
-newApp.get('/', function (req, res) {
-    res.send("New port is running on 4000")
-});*/
