a5b6305faa92b0f682fcbfc24f6256ed1ecc2665
Ashish <ashish240284@gmail.com>
2017-01-09 19:36:34 +0530

    post method adding file

--- new.js
+++ new.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var newApp = express();
 
 var port = 4000;
@@ -10,24 +10,23 @@ newApp.set('view engine', 'ejs');
 
 newRouter = express.Router();
 
-newRouter.param('name', function (req, res, next, name) {
-    console.log('doing name validation on '+ name);
-   req.name = name;
-    next();
-});
-newRouter.get('/handler/:name', function (req, res) {
-    res.send("Hello "+ req.name + '!');
-});
+newApp.use(bodyParser.json());
+newApp.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 newApp.get('/about', function (req, res) {
     res.render("pages/about");
+});
+newApp.get('/contact', function (req, res) {
+    res.render("pages/contact");
 });
 
 newApp.get('/', function (req, res) {
     var drinks = [
         {name: 'Coca Cola', drunkness: 1},
         {name: 'Pepsi', drunkness: 2},
-        {name: 'Thumsup', drunkness: 1}
+        {name: '7up', drunkness: 1}
     ];
     var tagline = 'Any code of your own that you haven';
 
@@ -37,6 +36,15 @@ newApp.get('/', function (req, res) {
         });
 });
 
+newApp.post('/sendData', function (req, res) {
+    var person = {
+        name: req.body.name,
+        contact: req.body.contact,
+        address: req.body.address
+    };
+
+    res.render('pages/dataReceive', person);
+});
 //about page
 
 newApp.use('/', newRouter);
